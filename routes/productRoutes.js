512b3b440b98f77d2c55db96cb13bae11b7c318b
Shreyas Agrawal <48771895+splendidbug@users.noreply.github.com>
2023-06-28 01:33:07 -0700

    added cors for axios

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const router = express.Router();
+router.use(cors());
+
 const {
   getProducts,
   createProduct,
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDb = require("./config/dbConnection");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 connectDb();
 app.use("/api/products", require("./routes/productRoutes"));
